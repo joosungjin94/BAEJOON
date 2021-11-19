172cefe76c7f665c8baae15d32872e76a7076014
tjdcl <tjdcl@DESKTOP-8N71VPL>
2021-11-19 13:40:57 +0900

    계산기 기본 기능 구현 완료

--- src/CalculatorExample.java
+++ src/CalculatorExample.java
@@ -8,26 +8,38 @@ public class CalculatorExample {
 		int num = scanner.nextInt();
 		System.out.println(num);
 		
-		System.out.print("사칙연산 기호 :");
-		String sybom = scanner.next();
-		System.out.println(sybom);
-		
-		System.out.print("두 번째 입력 값: ");
-		int num1 = scanner.nextInt();
-		System.out.println(num1);
-		
-		if(sybom.equals("+")) {
-			System.out.printf("덧셈 : %d + %d = %d\n",num, num1,(num+num1));
-		} else if(sybom.equals("-")) {
-			System.out.printf("뺼셈 : %d - %d = %d\n",num, num1,(num-num1));			
-		} else if(sybom.equals("*")) {
-			System.out.printf("곱하기 :%d * %d = %d\n",num, num1,(num*num1));			
-		} else if(sybom.equals("/")) {
-			System.out.printf("나눗셈 :%d / %d = %d\n",num, num1,(num/num1));
-		} else {
-			System.out.println("사칙 연산 기호가 아닙니다.");
+		int result =0;
+		while (true) {
+			System.out.print("사칙연산 기호 :");
+			String sybom = scanner.next();
+			System.out.println(sybom);
+			
+			if(sybom.equals("exit")) {
+				System.out.printf("최종 결과 값 = %s\n",result);
+				break;
+			}
+			
+			System.out.print("두 번째 입력 값: ");
+			int num1 = scanner.nextInt();
+			System.out.println(num1);
+			
+			
+			if (sybom.equals("+")) {
+				result += num1; 
+				System.out.printf("덧셈 : %d + %d = %d\n", num, num1, (num + num1));
+			} else if (sybom.equals("-")) {
+				result += num1;
+				System.out.printf("뺼셈 : %d - %d = %d\n", num, num1, (num - num1));
+			} else if (sybom.equals("*")) {
+				result += num1;
+				System.out.printf("곱하기 :%d * %d = %d\n", num, num1, (num * num1));
+			} else if (sybom.equals("/")) {
+				result += num1;
+				System.out.printf("나눗셈 :%d / %d = %d\n", num, num1, (num / num1));
+			} else {
+				System.out.println("사칙 연산 기호가 아닙니다.");
+			}
 		}
-		
 	}
 
 }
